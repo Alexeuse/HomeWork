8461b401996ce741b810575b4eb45797660fd963
Emelyanov Alex <alexeuse@gmail.com>
2023-01-26 17:18:30 +0300

    init

---
+++ Class_29/script.js
@@ -0,0 +1,21 @@
+// let numbers = (a, b, c) => a + b + c;
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// console.log numbers();
+
+// let a = +prompt('a');
+// let b = +prompt('b');
+// let c = +prompt('c');
+// 
+// let numbers = function (a, b, c) {
+// console.log(a + b + c);
+// 
+// 
+// 
+// }
+// numbers(a + b + c);
+
+
+
+
