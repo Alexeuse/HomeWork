26cae7638722460cfcaf15968b7e3742c0cea430
Emelyanov Alex <alexeuse@gmail.com>
2023-01-27 16:57:44 +0300

    init

--- Class_29/script.js
+++ Class_29/script.js
@@ -1,20 +1,62 @@
-// let numbers = (a, b, c) => a + b + c;
-// let a = 1;
-// let b = 2;
-// let c = 3;
-// console.log numbers();
-
+// Напишите функцию, которая получает три числа и возвращает их сумму.
 // let a = +prompt('a');
 // let b = +prompt('b');
 // let c = +prompt('c');
-// 
-// let numbers = function (a, b, c) {
+// let numbers = function () {
 // console.log(a + b + c);
-// 
-// 
-// 
 // }
-// numbers(a + b + c);
+// numbers();
+
+// Напишите функцию, которая подсчитывает сумму чисел от 1 до заданного X.
+// let a = +prompt('Enter number');
+// let summa = function () {
+// let b = 0;
+// let sum = 0;
+// while (b < a) {
+// b++;
+// sum += b
+// }
+// console.log(sum);
+// }
+// summa();
+
+
+//Напишите функцию, которая подсчитывает сумму цифр числа
+
+
+
+// let a = 22;
+// let c = 0;
+// let summa = function () {
+    // for (; a > 0; a = a - c) {
+        // c = a % 10;
+    // }
+// 
+// 
+    // console.log(c);
+// }
+// summa();
+
+
+
+// let x = prompt('Enter number');
+// let a = x[0] * 1;
+// let b = x[1] * 1;
+// let summ = a + b;
+// console.log(summ);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
